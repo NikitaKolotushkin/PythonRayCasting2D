2ebb9a0077a57d87c2173f32eb89afa76fd6c50e
Nikita_Ko <nikitaplay1000@gmail.com>
2020-07-03 12:39:13 +0400

    Create a base logic on main.py and settings.py files

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-123
+import pygame
+from settings import *
+
+pygame.init()
+sc = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    sc.fill(BLACK)
+
+    pygame.draw.circle(sc, GREEN, player_pos, 12)
+
+    pygame.display.flip()
+    clock.tick()
