3a49f80c0f29bdc98c3b9c05bfb12e51f5574847
Nikita_Ko <nikitaplay1000@gmail.com>
2020-07-03 13:05:04 +0400

    Add a game map

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from player import Player
 import math
+from map import world_map
 
 pygame.init()
 sc = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -19,6 +20,9 @@ while True:
     pygame.draw.circle(sc, GREEN, player.pos, 12)
     pygame.draw.line(sc, GREEN, player.pos, (player.x + WIDTH * math.cos(player.angle),
                                              player.y + WIDTH * math.sin(player.angle)))
+
+    for x, y in world_map:
+        pygame.draw.rect(sc, DARKGRAY, (x, y, TILE, TILE), 2)
 
     pygame.display.flip()
     clock.tick(FPS)
